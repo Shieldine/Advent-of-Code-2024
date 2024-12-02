1ca4de1102e001035675dd283f150a728ec472d4
Shieldine <74987363+Shieldine@users.noreply.github.com>
2024-12-02 10:00:37 +0100

    solve second day

---
+++ day_2.py
@@ -0,0 +1,67 @@
+def read_input():
+    reports = []
+
+    with open('inputs/day_2.txt') as f:
+        for line in f:
+            numbers = line.split(" ")
+
+            reports.append([int(number) for number in numbers])
+
+    return reports
+
+
+def check_reports(reports):
+    num = 0
+
+    for report in reports:
+        if check_report(report):
+            num += 1
+
+    return num
+
+
+def check_report(report):
+    last = None
+    decrease = report[0] > report[1]
+
+    for number in report:
+        if last is None:
+            last = number
+            continue
+
+        if decrease and number > last:
+            return False
+        if not decrease and number < last:
+            return False
+
+        if 1 <= abs(number - last) <= 3:
+            last = number
+            continue
+        else:
+            return False
+
+    return True
+
+
+def check_dampened(reports):
+    num = 0
+
+    for report in reports:
+        if check_report(report):
+            num += 1
+            continue
+        else:
+            for i, number in enumerate(report):
+                temp = report.copy()
+                temp = [element for index, element in enumerate(temp) if index != i]
+                if check_report(temp):
+                    num += 1
+                    break
+
+    return num
+
+
+r = read_input()
+
+print(check_reports(r))
+print(check_dampened(r))
