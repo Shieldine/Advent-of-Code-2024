16c8faff885eab64891c8aa922be80796a90801b
Shieldine <74987363+Shieldine@users.noreply.github.com>
2024-12-18 00:25:00 +0100

    wip on day 17 part 2

--- scripts/13-18/day_17.py
+++ scripts/13-18/day_17.py
@@ -1,9 +1,6 @@
 import re
 
-a = 0
-b = 0
-c = 0
-
+a = b = c = 0
 program = []
 
 with open("../../inputs/13-18/day_17.txt") as file:
@@ -14,14 +11,14 @@ with open("../../inputs/13-18/day_17.txt") as file:
             b = int(re.findall(r"\d+", line)[0])
         elif idx == 2:
             c = int(re.findall(r"\d+", line)[0])
-        elif idx == 3:
-            continue
         elif idx == 4:
-            program = line.replace("Program: ", "").strip().split(",")
+            program = [int(n) for n in line.replace("Program: ", "").strip().split(",")]
 
 
-def get_combo_operand(operand):
+def get_operand_value(operand, a, b, c):
     match operand:
+        case 0:
+            return 0
         case 1:
             return 1
         case 2:
@@ -34,45 +31,55 @@ def get_combo_operand(operand):
             return b
         case 6:
             return c
-        case 7:
+        case _:
             return None
 
 
-program = [int(program) for program in program]
-
-i = 0
-
-out = ''
-
-while i < len(program):
-    opcode = program[i]
-    operand = program[i + 1]
-
-    match opcode:
-        case 0:  # division
-            a = int(a / (2 ** get_combo_operand(operand)))
-        case 1:  # bitwise XOR
-            b = b ^ operand
-        case 2:  # bst
-            b = get_combo_operand(operand) % 8
-        case 3:  # jnz
-            if a == 0:
-                i += 2
-                continue
-            else:
-                i = operand
-                continue
-        case 4:  # bxc
-            b = b ^ c
-        case 5:  # out
-            out += str(get_combo_operand(operand) % 8) + ","
-        case 6:  # bdv
-            b = int(a / (2 ** get_combo_operand(operand)))
-        case 7:  # cdv
-            c = int(a / (2 ** get_combo_operand(operand)))
-
-    i += 2
-
-print(out[:-1])
+def execute_program(a, b, c, program):
+    i = 0
+    result = ''
+    while i < len(program):
+        opcode = program[i]
+        operand = program[i + 1]
+        match opcode:
+            case 0:
+                a = int(a / (2 ** get_operand_value(operand, a, b, c)))
+            case 1:
+                b = b ^ operand
+            case 2:
+                b = get_operand_value(operand, a, b, c) % 8
+            case 3:
+                if a == 0:
+                    i += 2
+                    continue
+                else:
+                    i = operand
+                    continue
+            case 4:
+                b = b ^ c
+            case 5:
+                result += str(get_operand_value(operand, a, b, c) % 8) + ","
+            case 6:
+                b = int(a / (2 ** get_operand_value(operand, a, b, c)))
+            case 7:
+                c = int(a / (2 ** get_operand_value(operand, a, b, c)))
+        i += 2
+    return result[:-1]
 
 
+print(execute_program(a, b, c, program))
+
+correct_a = 35184441564177
+
+while True:
+    output = execute_program(correct_a, b, c, program)
+
+    split = output.split(",")
+    split = [int(n) for n in split]
+
+    if split == program:
+        break
+
+    correct_a += 1
+
+print(correct_a)
