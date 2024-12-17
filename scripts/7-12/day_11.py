25b3c35076247edd27340d738aea2d867012ba84
Shieldine <74987363+Shieldine@users.noreply.github.com>
2024-12-17 15:35:59 +0100

    add memoisation coz this garbage is expensive

--- scripts/7-12/day_11.py
+++ scripts/7-12/day_11.py
@@ -1,25 +1,36 @@
 stones = []
-
 with open("../../inputs/7-12/day_11.txt") as file:
     stones = file.read().strip().split(" ")
 
 stones = [int(stone) for stone in stones]
 
-BLINKS = 75
+BLINKS = 25
+
+memo_cache = {}
+
+
+def transform(stone):
+    if stone in memo_cache:
+        return memo_cache[stone]
+
+    if stone == 0:
+        result = [1]
+    elif len(str(stone)) % 2 == 0:
+        digits = list(str(stone))
+        midpoint = len(digits) // 2
+        result = [int("".join(digits[:midpoint])), int("".join(digits[midpoint:]))]
+    else:
+        result = [stone * 2024]
+
+    memo_cache[stone] = result
+    return result
+
 
 for _ in range(BLINKS):
     new_list = []
 
     for stone in stones:
-        if stone == 0:
-            new_list.append(1)
-        elif len(str(stone)) % 2 == 0:
-            digits = list(str(stone))
-            midpoint = len(digits) // 2
-            new_list.append(int("".join(digits[:midpoint])))
-            new_list.append(int("".join(digits[midpoint:])))
-        else:
-            new_list.append(stone*2024)
+        new_list.extend(transform(stone))
 
     stones = new_list
 
