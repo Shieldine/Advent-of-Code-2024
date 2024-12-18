e41d90bf95debd0db8b9a7774cf4fb9e6e0ec0bc
Shieldine <74987363+Shieldine@users.noreply.github.com>
2024-12-18 20:40:24 +0100

    wip

--- scripts/13-18/day_15_2.py
+++ scripts/13-18/day_15_2.py
@@ -1,186 +1,153 @@
 from copy import deepcopy
 
-tile_map = []
-moves = []
-
-pos = []
-
-
-def print_map(tile_map):
-    tile_map[pos[0]][pos[1]] = "@"  # print current position
+def print_map(tile_map, pos):
+    tile_map[pos[0]][pos[1]] = "@"  # Mark the current position
     for row in tile_map:
         print(f'{"".join(row)}\n')
+    tile_map[pos[0]][pos[1]] = "."  # Reset the position marker
 
 
-with open("../../inputs/13-18/day_15.txt") as f:
-    mappy = True
-    row = 0
+def load_map_and_moves(file_path):
+    tile_map = []
+    moves = []
+    pos = []
 
-    for line in f:
-        if line == "\n":
-            mappy = False
-            continue
+    with open(file_path) as f:
+        mappy = True
+        row = 0
 
-        if mappy:
-            line = line.strip().replace("#", "##").replace(".", "..").replace("O", "[]").replace("@", '@.')
-            tile_map.append(line)
-            if "@" in line:
-                pos = [row, line.index("@")]
-        else:
-            moves.append(line.strip())
-        row += 1
+        for line in f:
+            if line == "\n":
+                mappy = False
+                continue
 
-    moves = "".join(moves)
+            if mappy:
+                line = line.strip().replace("#", "##").replace(".", "..").replace("O", "[]").replace("@", "@.")
+                tile_map.append(line)
+                if "@" in line:
+                    pos = [row, line.index("@")]
+            else:
+                moves.append(line.strip())
+            row += 1
 
-tile_map = [list(row) for row in tile_map]
+        moves = "".join(moves)
 
-# replace starting point with dot
-tile_map[pos[0]][pos[1]] = "."
+    tile_map = [list(row) for row in tile_map]
+    tile_map[pos[0]][pos[1]] = "."  # Replace starting point with a dot
+    return tile_map, moves, pos
 
-print_map(deepcopy(tile_map))
 
-
-def find_boxes(box, direction):
+def find_boxes(box, direction, tile_map):
     positions = []
 
     for part in box:
-        if tile_map[part[0] + direction][part[1]] == "#":
-            print("cant move")
+        next_tile = tile_map[part[0] + direction][part[1]]
+        if next_tile == "#":
             return -1
-        if tile_map[part[0] + direction][part[1]] == "[" or tile_map[part[0] + direction][part[1]] == "]":
-            first = (box[0] + direction, box[1])
-            if tile_map[part[0] + direction][part[1]] == "[":
-                second = (box[0] + direction, box[1] + 1)
-            else:
-                second = (box[0] + direction, box[1] + -1)
+        elif next_tile in ["[", "]"]:
+            first = (part[0] + direction, part[1])
+            second = (part[0] + direction, part[1] + 1) if next_tile == "[" else (part[0] + direction, part[1] - 1)
             positions.append(first)
             positions.append(second)
-
-            positions.append(find_boxes([first, second], direction))
+            nested_boxes = find_boxes([first, second], direction, tile_map)
+            if nested_boxes == -1:
+                return -1
+            positions.extend(nested_boxes)
 
     return positions
 
 
-for move in moves:
-    if move == "<":  # left
-        next_pos = (pos[0], pos[1] - 1)
-    elif move == "^":  # up
-        next_pos = (pos[0] - 1, pos[1])
-    elif move == ">":  # right
-        next_pos = (pos[0], pos[1] + 1)
-    elif move == "v":  # down
-        next_pos = (pos[0] + 1, pos[1])
-    else:
-        continue
+def process_moves(tile_map, moves, pos):
+    for move in moves:
+        next_pos = {
+            "<": (pos[0], pos[1] - 1),
+            "^": (pos[0] - 1, pos[1]),
+            ">": (pos[0], pos[1] + 1),
+            "v": (pos[0] + 1, pos[1]),
+        }.get(move, pos)
 
-    x, y = next_pos
+        x, y = next_pos
+        if tile_map[x][y] in ["[", "]"]:
+            positions_to_move = [(x, y)]
+            if tile_map[x][y] == "[":
+                positions_to_move.append((x, y + 1))
+            else:
+                positions_to_move.append((x, y - 1))
 
-    if tile_map[x][y] == "[" or tile_map[x][y] == "]":
+            end = x, y
+            valid = True
 
-        # to keep track of all affected boxes
-        positions_to_move = [(x, y)]
-
-        # second part of the box
-        if tile_map[x][y] == "[":
-            positions_to_move.append((x, y + 1))
-        else:
-            positions_to_move.append((x, y - 1))
-
-        end = x, y
-        valid = True
-        match move:
-            case "<":
-                # all da boxes to da left
-                while tile_map[end[0]][end[1] - 1] == "[" or tile_map[end[0]][end[1] - 1] == "]":
-                    if tile_map[end[0]][end[1] - 1] == "]":
-                        end = end[0], end[1] - 1
-                        positions_to_move.append((end[0], end[1] - 1))
-                        positions_to_move.append((end[0], end[1] - 2))
-                    if tile_map[end[0]][end[1] - 1] == "[":
-                        end = end[0], end[1] - 1
-                if tile_map[end[0]][end[1] - 1] == "#":
-                    valid = False
-                end = end[0], end[1] - 1
-            case "^":
-                # all da boxes up
-                if tile_map[end[0] - 1][end[1]] == "[" or tile_map[end[0] - 1][end[1]] == "]":
-                    first = (end[0] - 1, end[1])
-                    if tile_map[end[0] - 1][end[1]] == "[":
-                        second = (end[0] - 1, end[1] + 1)
-                    else:
-                        second = (end[0] - 1, end[1] - 1)
-                    boxes = find_boxes([first, second], -1)
-
-                    if boxes == -1:
-                        valid = False
-                    else:
-                        positions_to_move.append(first)
-                        positions_to_move.append(second)
-                        positions_to_move.append(boxes)
-                if tile_map[end[0] - 1][end[1]] == "#":
-                    valid = False
-            case ">":
-                # all da boxes to da right
-                while tile_map[end[0]][end[1] + 1] == "[" or tile_map[end[0]][end[1] + 1] == "]":
-                    if tile_map[end[0]][end[1] + 1] == "[":
-                        end = end[0], end[1] + 1
-                        positions_to_move.append((end[0], end[1] + 1))
-                        positions_to_move.append((end[0], end[1] + 2))
-                    if tile_map[end[0]][end[1] + 1] == "]":
-                        end = end[0], end[1] + 1
-                if tile_map[end[0]][end[1] + 1] == "#":
-                    valid = False
-                end = end[0], end[1] + 1
-            case "v":
-                # all da boxes down!
-                if tile_map[end[0] + 1][end[1]] == "[" or tile_map[end[0] + 1][end[1]] == "]":
-                    first = (end[0] + 1, end[1])
-                    if tile_map[end[0] + 1][end[1]] == "[":
-                        second = (end[0] + 1, end[1] + 1)
-                    else:
-                        second = (end[0] + 1, end[1] - 1)
-                    boxes = find_boxes([first, second], 1)
-
-                    if boxes == -1:
-                        valid = False
-                    else:
-                        positions_to_move.append(first)
-                        positions_to_move.append(second)
-                        positions_to_move.append(boxes)
-                end = end[0] + 1, end[1]
-
-        if valid:
-            pos = [x, y]
-            direction = (0, 0)
             match move:
                 case "<":
-                    direction = (0, -1)
-                    positions_to_move = sorted(positions_to_move, key=lambda tup: tup[1])
+                    while tile_map[end[0]][end[1] - 1] in ["[", "]"]:
+                        if tile_map[end[0]][end[1] - 1] == "]":
+                            positions_to_move.append((end[0], end[1] - 1))
+                            positions_to_move.append((end[0], end[1] - 2))
+                        end = (end[0], end[1] - 1)
+                    if tile_map[end[0]][end[1] - 1] == "#":
+                        valid = False
                 case "^":
-                    direction = (-1, 0)
-                    positions_to_move = sorted(positions_to_move, key=lambda tup: tup[0])
+                    if tile_map[end[0] - 1][end[1]] in ["[", "]"]:
+                        first = (end[0] - 1, end[1])
+                        second = (end[0] - 1, end[1] + 1) if tile_map[end[0] - 1][end[1]] == "[" else (end[0] - 1, end[1] - 1)
+                        nested_boxes = find_boxes([first, second], -1, tile_map)
+                        if nested_boxes == -1:
+                            valid = False
+                        else:
+                            positions_to_move.extend([first, second, *nested_boxes])
+                    if tile_map[end[0] - 1][end[1]] == "#":
+                        valid = False
                 case ">":
-                    direction = (0, 1)
-                    positions_to_move = sorted(positions_to_move, key=lambda tup: tup[1])[::-1]
+                    while tile_map[end[0]][end[1] + 1] in ["[", "]"]:
+                        if tile_map[end[0]][end[1] + 1] == "[":
+                            positions_to_move.append((end[0], end[1] + 1))
+                            positions_to_move.append((end[0], end[1] + 2))
+                        end = (end[0], end[1] + 1)
+                    if tile_map[end[0]][end[1] + 1] == "#":
+                        valid = False
                 case "v":
-                    direction = (1, 0)
-                    positions_to_move = sorted(positions_to_move, key=lambda tup: tup[0])[::-1]
+                    if tile_map[end[0] + 1][end[1]] in ["[", "]"]:
+                        first = (end[0] + 1, end[1])
+                        second = (end[0] + 1, end[1] + 1) if tile_map[end[0] + 1][end[1]] == "[" else (end[0] + 1, end[1] - 1)
+                        nested_boxes = find_boxes([first, second], 1, tile_map)
+                        if nested_boxes == -1:
+                            valid = False
+                        else:
+                            positions_to_move.extend([first, second, *nested_boxes])
+                    if tile_map[end[0] + 1][end[1]] == "#":
+                        valid = False
 
-            for position in positions_to_move:
-                tile_map[position[0] + direction[0]][position[1] + direction[1]] = tile_map[position[0]][position[1]]
-                tile_map[position[0] + direction[0]][position[1]] = "."
+            if valid:
+                direction = {
+                    "<": (0, -1),
+                    "^": (-1, 0),
+                    ">": (0, 1),
+                    "v": (1, 0),
+                }[move]
+                positions_to_move.sort(key=lambda tup: (tup[0], tup[1]), reverse=(move in [">", "v"]))
 
-    elif tile_map[x][y] != "#":
-        pos = [x, y]
+                for position in positions_to_move:
+                    dx, dy = direction
+                    tile_map[position[0] + dx][position[1] + dy] = tile_map[position[0]][position[1]]
+                    tile_map[position[0]][position[1]] = "."
+                pos = [x, y]
 
-    print(move)
-    print_map(deepcopy(tile_map))
+        elif tile_map[x][y] != "#":
+            pos = [x, y]
 
-score = 0
+        print(move)
+        print_map(deepcopy(tile_map), pos)
 
-for idx, row in enumerate(tile_map):
-    for idx_c, col in enumerate(row):
-        if col == "[":  # NEW: score is calculated by the left side of a box
-            score += idx * 100 + idx_c
+    return tile_map
 
+
+def calculate_score(tile_map):
+    return sum(idx * 100 + idx_c for idx, row in enumerate(tile_map) for idx_c, col in enumerate(row) if col == "[")
+
+
+file_path = "../../inputs/13-18/day_15.txt"
+tile_map, moves, pos = load_map_and_moves(file_path)
+print_map(deepcopy(tile_map), pos)
+tile_map = process_moves(tile_map, moves, pos)
+score = calculate_score(tile_map)
 print(score)
