9b5c73f46c789b34e17164e918921dc44ef15e51
Shieldine <74987363+Shieldine@users.noreply.github.com>
2024-12-20 17:17:26 +0100

    modify to show the path taken

--- scripts/13-18/day_16.py
+++ scripts/13-18/day_16.py
@@ -18,18 +18,21 @@ def solve_maze(maze):
     def heuristic(pos, end):
         return abs(pos[0] - end[0]) + abs(pos[1] - end[1])
 
-    # priority queue: (total_cost, current_cost, position, direction)
-    # direction is an index into `directions` (0=N, 1=E, 2=S, 3=W)
+    # Priority queue: (total_cost, current_cost, position, direction, path)
     pq = []
-    heapq.heappush(pq, (0, 0, start, 1))
+    heapq.heappush(pq, (0, 0, start, 1, []))
 
     visited = set()
 
     while pq:
-        total_cost, current_cost, (y, x), direction = heapq.heappop(pq)
+        total_cost, current_cost, (y, x), direction, path = heapq.heappop(pq)
 
         if (y, x) == end:
-            return total_cost
+            full_path = path + [(y, x)]
+            for (py, px) in full_path:
+                if maze[py][px] != 'S' and maze[py][px] != 'E':
+                    maze[py][px] = '+'
+            return total_cost, full_path, maze
 
         if (y, x, direction) in visited:
             continue
@@ -42,12 +45,21 @@ def solve_maze(maze):
                 move_cost = 1
                 turn_cost = 1000 if i != direction else 0
                 new_cost = current_cost + move_cost + turn_cost
-                heapq.heappush(pq, (new_cost + heuristic((ny, nx), end), new_cost, (ny, nx), i))
+                new_path = path + [(y, x)]  # Add current position to path
+                heapq.heappush(pq, (new_cost + heuristic((ny, nx), end), new_cost, (ny, nx), i, new_path))
 
-    return -1
+    return -1, [], maze
+
+
+def print_maze(maze):
+    for row in maze:
+        print(''.join(row))
 
 
 with open("../../inputs/13-18/day_16.txt") as f:
     maze = f.read()
 
-print(solve_maze(maze))
+cost, path, maze_with_path = solve_maze(maze)
+print(f"Cost to reach the end: {cost}")
+print("\nMaze with path:")
+print_maze(maze_with_path)
