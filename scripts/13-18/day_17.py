a1467cb31c4d1b1e5cef81e9b2e089ec69abf492
Shieldine <74987363+Shieldine@users.noreply.github.com>
2025-01-11 20:28:45 +0100

    move notes to the day itself

--- scripts/13-18/day_17.py
+++ scripts/13-18/day_17.py
@@ -85,6 +85,33 @@ print(execute_program(a, b, c, program))
 #
 # print(correct_a)
 
+"""
+We have: 2,4 1,3 7,5 1,5 0,3 4,3 5,5 3,0
+
+What happens, step by step:
+
+- b = a % 8
+- b = b ^ 3
+- c = a >> b
+- b = b ^ 5
+- a = a >> 3
+- b = b ^ c
+- output b % 8
+- loop if a != 0
+
+a needs to be large enough to produce 16 numbers - so big.
+
+we divide a by 8 in each loop (3 Bits shift), a needs to be 0 in the last one
+since we output b and want it to output the input, b needs to assume the numbers of the input step by step.
+we need to go backwards somehow
+
+so, with each step, we will have to multiply with 8 (since we divided earlier), but since we shifted, we lost some
+value and don't know which (it will be between 0 and 7 though since we did 3 bits)
+
+that means with each step, we will have to check the current value multiplied with 8 plus something between 0 and 7 - 
+and we have to check them all. we know it ended with a 0 so we'll start with a 0 and try to recreate the input starting
+from the end
+"""
 
 candidates = [0]
 
