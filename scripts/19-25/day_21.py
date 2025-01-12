8060df35ebf0919ffb632f069aaabb81b5dfd9c7
Shieldine <74987363+Shieldine@users.noreply.github.com>
2025-01-12 23:27:24 +0100

    refactor

--- scripts/19-25/day_21.py
+++ scripts/19-25/day_21.py
@@ -8,15 +8,48 @@ with open("../../inputs/19-25/day_21.txt") as f:
 sequences = [list(sequence) for sequence in sequences]
 
 
-class NumericalKeypad:
+class KeypadBase:
+    def __init__(self, keypad, position):
+        self.keypad = keypad
+        self.position = position
+
+    def move_vertically(self, way, pos):
+        dx = pos[0] - self.position[0]
+        direction = -1, "^"
+
+        if dx > 0:
+            direction = 1, "v"
+
+        for i in range(abs(dx)):
+            nx = self.position[0] + direction[0]
+
+            way.append(direction[1])
+            self.position = (nx, self.position[1])
+
+    def move_sideways(self, way, pos):
+        dy = pos[1] - self.position[1]
+        direction = -1, "<"
+
+        if dy > 0:
+            direction = 1, ">"
+
+        for i in range(abs(dy)):
+            ny = self.position[1] + direction[0]
+            way.append(direction[1])
+            self.position = (self.position[0], ny)
+
+
+class NumericalKeypad(KeypadBase):
     def __init__(self):
-        self.keypad = [
-            ["7", "8", "9"],
-            ["4", "5", "6"],
-            ["1", "2", "3"],
-            [None, "0", "A"]
-        ]
-        self.position = (3, 2)
+        super().__init__(
+            [
+                ["7", "8", "9"],
+                ["4", "5", "6"],
+                ["1", "2", "3"],
+                [None, "0", "A"]
+            ],
+            (3, 2)
+        )
 
     def press_button(self, key):
         way = []
@@ -40,69 +73,25 @@ class NumericalKeypad:
             up_down_first = True
 
         if up_down_first:
-            # up/down
-            dx = pos[0] - self.position[0]
-            direction = -1, "^"
-
-            if dx > 0:
-                direction = 1, "v"
-
-            for i in range(abs(dx)):
-                nx = self.position[0] + direction[0]
-
-                way.append(direction[1])
-                self.position = (nx, self.position[1])
-
-            # left/right
-            dy = pos[1] - self.position[1]
-            direction = -1, "<"
-
-            if dy > 0:
-                direction = 1, ">"
-
-            for i in range(abs(dy)):
-                ny = self.position[1] + direction[0]
-
-                way.append(direction[1])
-                self.position = (self.position[0], ny)
+            self.move_vertically(way, pos)
+            self.move_sideways(way, pos)
         else:
-            # left/right
-            dy = pos[1] - self.position[1]
-            direction = -1, "<"
-
-            if dy > 0:
-                direction = 1, ">"
-
-            for i in range(abs(dy)):
-                ny = self.position[1] + direction[0]
-
-                way.append(direction[1])
-                self.position = (self.position[0], ny)
-            # up/down
-            dx = pos[0] - self.position[0]
-            direction = -1, "^"
-
-            if dx > 0:
-                direction = 1, "v"
-
-            for i in range(abs(dx)):
-                nx = self.position[0] + direction[0]
-
-                way.append(direction[1])
-                self.position = (nx, self.position[1])
+            self.move_sideways(way, pos)
+            self.move_vertically(way, pos)
 
         way.append("A")
         return way
 
 
-class DirectionalKeypad:
+class DirectionalKeypad(KeypadBase):
     def __init__(self):
-        self.keypad = [
-            [None, "^", "A"],
-            ["<", "v", ">"]
-        ]
-
-        self.position = (0, 2)
+        super().__init__(
+            [
+                [None, "^", "A"],
+                ["<", "v", ">"]
+            ],
+            (0, 2)
+        )
 
     def press_button(self, key):
         way = []
@@ -113,72 +102,22 @@ class DirectionalKeypad:
                 if col == key:
                     pos = (idx, idx_c)
 
-        # check if we'd run into None
-        if self.position[0] == 0 and pos == (1, 0):
-            way.append("v")
-            self.position = (self.position[0] + 1, self.position[1])
-
-        # decide if we should go left/right or up/down first
         up_down_first = False
 
         # that's if we'd run into the None
         if self.position[0] == 0 and pos == (0, 1):
             up_down_first = True
+
         # here we prioritise up and down over right
         if (pos[1] - self.position[1]) > 0:
             up_down_first = True
 
         if up_down_first:
-            # up/down
-            dx = pos[0] - self.position[0]
-            direction = -1, "^"
-
-            if dx > 0:
-                direction = 1, "v"
-
-            for i in range(abs(dx)):
-                nx = self.position[0] + direction[0]
-
-                way.append(direction[1])
-                self.position = (nx, self.position[1])
-
-            # left-right
-            dy = pos[1] - self.position[1]
-            direction = -1, "<"
-
-            if dy > 0:
-                direction = 1, ">"
-
-            for i in range(abs(dy)):
-                ny = self.position[1] + direction[0]
-                way.append(direction[1])
-                self.position = (self.position[0], ny)
+            self.move_vertically(way, pos)
+            self.move_sideways(way, pos)
         else:
-            # left-right
-            dy = pos[1] - self.position[1]
-            direction = -1, "<"
-
-            if dy > 0:
-                direction = 1, ">"
-
-            for i in range(abs(dy)):
-                ny = self.position[1] + direction[0]
-                way.append(direction[1])
-                self.position = (self.position[0], ny)
-
-            # up/down
-            dx = pos[0] - self.position[0]
-            direction = -1, "^"
-
-            if dx > 0:
-                direction = 1, "v"
-
-            for i in range(abs(dx)):
-                nx = self.position[0] + direction[0]
-
-                way.append(direction[1])
-                self.position = (nx, self.position[1])
-
+            self.move_sideways(way, pos)
+            self.move_vertically(way, pos)
 
         way.append("A")
         return way
@@ -195,19 +134,19 @@ all_presses = {}
 for sequence in sequences:
     presses = sequence
 
-    for idx, keypad in enumerate(all_keypads):
+    for cur_keypad in all_keypads:
         new_presses = []
 
-        for key in presses:
-            new_presses.extend(keypad.press_button(key))
+        for cur_key in presses:
+            new_presses.extend(cur_keypad.press_button(cur_key))
 
         presses = new_presses
     all_presses["".join(sequence)] = presses.copy()
 
 score = 0
 
-for key, val in all_presses.items():
-    print(key, len(val), "".join(val))
-    score += len(val) * int(key[:-1])
+for cur_key, val in all_presses.items():
+    print(cur_key, len(val), "".join(val))
+    score += len(val) * int(cur_key[:-1])
 
 print(score)
